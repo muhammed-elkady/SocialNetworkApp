0ce06463ad1d9062ed9bd52bebb5deafb04f6baa
Muhammed El Kady <muhammed.elkady@flairstech.com>
2020-11-19 19:20:30 +0200

    added call to values controller from client

--- social-network-app-client/src/App.tsx
+++ social-network-app-client/src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+  componentDidMount() {
+    axios.get('https://localhost:44339/api/values')
+      .then(res => {
+        this.setState(
+          {
+            values: res.data
+          }
+        )
+      })
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            {this.state.values.map((v: any) =>
+              (
+                <li>{v.name}</li>
+              )
+            )}
+          </ul>
+        </header>
+      </div>
+
+    );
+  }
 }
 
 export default App;
